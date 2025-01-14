5960162c28d30489f3cd92fe30830b8ea05330c1
dericko681 <abonghoderick@gmail.com>
2025-01-14 20:43:10 +0100

    update: removedsudo
    Signed-off-by: dericko681 <abonghoderick@gmail.com>

--- task1.sh
+++ task1.sh
@@ -3,7 +3,7 @@
 # Check if acpi command is installed and install if not
 if  ! command -v acpi &> /dev/null; then
 echo "installing acpi"
-sudo apt-get update && sudo apt-get install -y acpi
+apt-get update && apt-get install -y acpi
 else
 echo "acpi command exist"
 fi
