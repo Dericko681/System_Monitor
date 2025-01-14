82fc22433594d9927c864999cc405e362b8984d7
micheal-ndoh <michaelndoh9@gmail.com>
2025-01-14 07:24:11 +0100

    Added a new line space

--- task1.sh
+++ task1.sh
@@ -4,7 +4,6 @@ exec &>> >(tee -a "$LOG_FILE")
 # Check memory
 echo "Memory Information:"
 echo "$(free )"
-
 printf "\n"
 printf "\n"
 
@@ -17,11 +16,12 @@ printf "\n"
 # Check Processor temperature
 echo "Processor Temperature:"
 echo "$(acpi -t )"
-
 printf "\n"
 printf "\n"
 
 # Check CPU usage
 cpu=$(echo "CPU $(LC_ALL=C top -bn1 | grep "Cpu(s)" | sed "s/.*, *\([0-9.]*\)%* id.*/\1/" | awk '{print 100 - $1}')% RAM $(free -m | awk '/Mem:/ { printf("%3.1f%%", $3/$2*100) }')" | cut -d' ' -f2 )
 echo "CPU Usage: $cpu"
- 
+printf "\n" 
+printf "\n" 
+
